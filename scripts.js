08321944e41e7e3a79278aaa0c9639e819a13031
Monika N <monia.niemczyk1@gmail.com>
2018-04-11 18:48:34 +0200

    Add 'length' and function 'slice()'

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
 var dinosaur = "Triceratops";
 console.log(text);
+
 var dinosaurUpperCassed = dinosaur.toUpperCase();
 console.log(dinosaurUpperCassed);
-var dinosaurReplace = text.replace("Velociraptor", dinosaur);
+
+var dinosaurReplace = text.replace("Velociraptor", dinosaurUpperCassed);
 console.log(dinosaurReplace);
+console.log(dinosaurReplace.length);
+console.log(dinosaurReplace.slice(0,72));
